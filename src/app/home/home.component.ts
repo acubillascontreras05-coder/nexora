bd27ae6fb0b62b83892041a8d18617b8af34926f
Adrian <acubillsacontreras.05@gmail.com>
2026-08-11 01:13:35 -0500

    Menu movil con hamburguesa y fondo animado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { SERVICIOS, Servicio } from '../data/services.data';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: '../app.css'
 })
-export class HomeComponent {}
+export class HomeComponent {
+  servicios: Servicio[] = SERVICIOS;
+  indiceCarrusel = 0;
+  porPagina = 3;
+
+  get serviciosVisibles(): Servicio[] {
+    return this.servicios.slice(this.indiceCarrusel, this.indiceCarrusel + this.porPagina);
+  }
+
+  get puedeRetroceder(): boolean {
+    return this.indiceCarrusel > 0;
+  }
+
+  get puedeAvanzar(): boolean {
+    return this.indiceCarrusel + this.porPagina < this.servicios.length;
+  }
+
+  anterior(): void {
+    if (this.puedeRetroceder) {
+      this.indiceCarrusel--;
+    }
+  }
+
+  siguiente(): void {
+    if (this.puedeAvanzar) {
+      this.indiceCarrusel++;
+    }
+  }
+}
